d325d6e9784808c5087180e2cf7df7a9989f6b0b
Ricardo Chitagu <ricardo.everest@yahoo.com>
2019-10-06 09:02:15 +0200

    home page footer

--- home/js/main.js
+++ home/js/main.js
@@ -67,22 +67,9 @@ jQuery(document).ready(function($) {
 
 });
 
-(function($) {
-  "use strict"; 
 
-/* Preloader */
-$(window).on('load', function() {
-  var preloaderFadeOutTime = 500;
-  function hidePreloader() {
-    var preloader = $('.spinner-wrapper');
-    setTimeout(function() {
-      preloader.fadeOut(preloaderFadeOutTime);
-    }, 500);
-  }
-  hidePreloader();
-});
 
-});
+
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
